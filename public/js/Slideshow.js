ee0db8a5ec9bdbf978e84946dd0f0e17dd7cdbc8
Joonas <joonas.muukkonen@gmail.com>
2018-05-02 19:38:20 +0300

    slidshow animation

--- public/js/Slideshow.js
+++ public/js/Slideshow.js
@@ -5,10 +5,10 @@ function showSlides() {
     var i;
     var slides = document.getElementsByClassName("poster-image");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.opacity = 0;
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 2000); 
+    slides[slideIndex-1].style.opacity = 1;
+    setTimeout(showSlides, 5000); 
 }
