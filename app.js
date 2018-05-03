3bad42cb6f2ff1f6299708a113ec86ab34338d00
joonas <joonas.muukkonen@gmail.com>
2018-05-03 08:42:58 +0300

    commenting app.js

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var express = require('express');
 
 var app = express();
 
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-var routes = require('./routes');
-
+// path joining for ejs, so public directory containing images, js and css can be used
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
+var routes = require('./routes');
+
 app.get('/', routes.home);
 app.get('/:microphone_name?', routes.microphone_info);
 app.get('*', routes.notFound);
 
+// start the web-app at localhost:3000
 app.listen(3000, function() {
-	console.log("The applictaion is running on localhost:3000");
+	console.log("The application is running on localhost:3000");
 });
